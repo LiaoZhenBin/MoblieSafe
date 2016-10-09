18559cbaa67d4ba9d5d0dcb4a1492c1c303e7453
liaozhenbin <liaozhenbin@wind-mobi.com>
2016-10-09 15:18:06 +0800

    完善启动界面
     Changes to be committed:
    	new file:   .idea/dictionaries/liaozhenbin.xml
    	modified:   .idea/gradle.xml
    	modified:   .idea/libraries/appcompat_v7_24_2_1.xml
    	new file:   .idea/libraries/hamcrest_core_1_3.xml
    	new file:   .idea/libraries/junit_4_12.xml
    	modified:   .idea/libraries/support_compat_24_2_1.xml
    	modified:   .idea/libraries/support_core_ui_24_2_1.xml
    	modified:   .idea/libraries/support_fragment_24_2_1.xml
    	modified:   .idea/libraries/support_media_compat_24_2_1.xml
    	modified:   app/src/main/AndroidManifest.xml
    	new file:   app/src/main/java/com/liaozhenbin/mobliesafe/activity/IndexActivity.java
    	modified:   app/src/main/java/com/liaozhenbin/mobliesafe/activity/SplashActivity.java
    	new file:   app/src/main/java/com/liaozhenbin/mobliesafe/utils/StreamUtil.java
    	new file:   app/src/main/res/layout/activity_index.xml
    	modified:   app/src/main/res/layout/activity_splash.xml
    	modified:   build.gradle
    	modified:   gradle/wrapper/gradle-wrapper.properties
    	new file:   projectFilesBackup/.idea/workspace.xml

--- app/src/main/java/com/liaozhenbin/mobliesafe/activity/SplashActivity.java
+++ app/src/main/java/com/liaozhenbin/mobliesafe/activity/SplashActivity.java
@@ -3,7 +3,10 @@ package com.liaozhenbin.mobliesafe.activity;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
@@ -22,25 +25,122 @@ public class SplashActivity extends AppCompatActivity {
     private ImageView icon;
     private ImageView wallet;
 
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            startAnim();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        icon = (ImageView) findViewById(R.id.iv_icon);
-        wallet = (ImageView) findViewById(R.id.iv_wallet);
-
-        startIcon();
-        initWallet();
+        initUI();
+        startAnim();
+        splashing();
 
     }
 
+
+    /**
+     * play two times animation
+     */
+    private void splashing() {
+        new Thread() {
+            @Override
+            public void run() {
+                Message msg = mHandler.obtainMessage();
+                long startTime = System.currentTimeMillis();
+                try {
+                    mHandler.sendMessageDelayed(msg, 1500);
+                } finally {
+                    long endTime = System.currentTimeMillis();
+                    if (endTime - startTime < 3500) {
+                        try {
+                            Thread.sleep(3500 - (endTime - startTime));
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    startActivity(new Intent(SplashActivity.this,IndexActivity.class));
+                    finish();
+                }
+
+            }
+        }.start();
+    }
+
+//    /**
+//     * connect intent to check the version
+//     */
+//    private void getData() {
+//        new Thread() {
+//
+//            Message msg = new Message();
+//
+//            @Override
+//            public void run() {
+//                try {
+//                    URL url = new URL("http://www.weather.com.cn/data/cityinfo/101190404.html");
+//
+//                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//                    //3,设置常见请求参数(请求头)
+//
+//                    //请求超时
+//                    connection.setConnectTimeout(4000);
+//                    //读取超时
+//                    connection.setReadTimeout(4000);
+//
+//                    //默认就是get请求方式,
+//                    //connection.setRequestMethod("POST");
+//
+//                    //4,获取请求成功响应码
+//                    if(connection.getResponseCode() == 200){
+//                        //5,以流的形式,将数据获取下来
+//                        InputStream is = connection.getInputStream();
+//                        //6,将流转换成字符串(工具类封装)
+//                        String json = StreamUtil.streamToString(is);
+//
+//                        Log.d("Liao",json+"____");
+//
+//                        msg.what = 1;
+//                        msg.obj = json;
+//                        mHandler.sendMessage(msg);
+//                    }
+//                } catch (MalformedURLException e) {
+//                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }.start();
+//    }
+
+    private void initUI() {
+        icon = (ImageView) findViewById(R.id.iv_icon);
+        wallet = (ImageView) findViewById(R.id.iv_wallet);
+    }
+
+
+    /**
+     * start splash animation
+     */
+
+
+    private void startAnim() {
+        startIcon();
+        initWallet();
+    }
+
+
     private void initWallet() {
-        final ValueAnimator  valueAnimator = ValueAnimator.ofFloat(0,1);
+        final ValueAnimator valueAnimator = ValueAnimator.ofFloat(0, 1);
         valueAnimator.setDuration(2000);
         valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator animation) {
-                if(animation.getAnimatedFraction() >= 0.7){
+                if (animation.getAnimatedFraction() >= 0.55) {
                     valueAnimator.cancel();
                     startWallet();
                 }
@@ -49,6 +149,9 @@ public class SplashActivity extends AppCompatActivity {
         valueAnimator.start();
     }
 
+    /**
+     * start wallet animation
+     */
     private void startWallet() {
         icon.setVisibility(View.INVISIBLE);
 
@@ -67,17 +170,18 @@ public class SplashActivity extends AppCompatActivity {
     }
 
 
-
+    /**
+     * icon start animation
+     */
     private void startIcon() {
+        icon.setVisibility(View.VISIBLE);
         //掉落动画
-        TranslateAnimation translate = (TranslateAnimation) AnimationUtils.loadAnimation(this,R.anim.translate_anim);
-        translate.setDuration(2000);
+        TranslateAnimation translate = (TranslateAnimation) AnimationUtils.loadAnimation(this, R.anim.translate_anim);
+        translate.setDuration(1400);
         //旋转动画
         Animation rotate = new MyAnimation();
-        rotate.setDuration(1000);
-        rotate.setRepeatCount(4);
+        rotate.setRepeatCount(10);
         AnimationSet animationSet = new AnimationSet(true);
-        animationSet.setDuration(1500);
         animationSet.addAnimation(rotate);
         animationSet.addAnimation(translate);
         icon.startAnimation(animationSet);
